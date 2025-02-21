057390e3973c072c520c0d915d2345537bc0c6bc
MrPinacolada <vasiasko112@gmail.com>
2025-02-21 23:05:27 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  plugins: ['~/plugins/touch.ts']
-})
+  plugins: ["~/plugins/touch.ts"],
+  app: {
+    baseURL: "/nuxt-touch-test/",
+  },
+  router: {
+    base: "/nuxt-touch-test/",
+  },
+  target: "static",
+  ssr: false,
+});
